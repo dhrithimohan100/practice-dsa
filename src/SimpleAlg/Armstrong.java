d90a232a370f248155239ab368d2f79bbcfcbade
DHRITHI MOHAN <dhrithim19@gmail.com>
2025-11-25 23:37:09 +0530

    new2codes

---
+++ src/SimpleAlg/Armstrong.java
@@ -0,0 +1,21 @@
+package SimpleAlg;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        int num = 370;//153
+        int original = num;
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit * digit * digit;
+            num = num / 10;
+        }
+        if (sum == original) {
+            System.out.println("the number is an armstrong number");
+        } else {
+            System.out.println("the number is not an armstrong number");
+        }
+    }
+}
+
